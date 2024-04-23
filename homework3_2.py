90fc65b055621e0f61bc836009fd130c6147927a
Sv-Dan1k <164805682+Sv-Dan1k@users.noreply.github.com>
2024-04-23 16:34:56 +0300

    Update homework3_2.py

--- homework3_2.py
+++ homework3_2.py
@@ -1,11 +1,15 @@
 import random
 
+print("Provide 5 numbers from 1 to 36 (one by one - separated by spacebars):")
+ticket_input: list = list(map(int, input().split()))
 
-def get_numbers_ticket(min=1, max=36, quantity=5):
+
+def get_numbers_ticket(min, max, quantity):
+    lottery_list = []
+    if len(ticket_input) != 5:
+        return print(lottery_list)
     ticket_lottery = random.sample(range(min, max), quantity)
     ticket_lottery.sort()
-    print("Provide 5 numbers from 1 to 36 (one by one - separated by spacebars):")
-    ticket_input: list = list(map(int, input().split()))
     result = list(zip(ticket_lottery, ticket_input))
     print(f'Lottery Ticket: {" ".join([str(i) for i in ticket_lottery])}')
     print(f'User\'s Choice : {" ".join([str(i) for i in ticket_input])}')
@@ -15,7 +19,9 @@ def get_numbers_ticket(min=1, max=36, quantity=5):
         if lottery == user_choice:
             points += 1
     print('\nTotal points', points)
+    lottery_list.append(result)
+    return  lottery_list
 
 
-get_numbers_ticket()
+get_numbers_ticket(min=1, max=36, quantity=5)
 
