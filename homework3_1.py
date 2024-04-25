1909910341fdc49507c9d950e5523da4f4127cd5
Sv-Dan1k <164805682+Sv-Dan1k@users.noreply.github.com>
2024-04-26 00:17:05 +0300

    Update homework3_1.py

--- homework3_1.py
+++ homework3_1.py
@@ -4,8 +4,9 @@ from datetime import datetime
 def get_days_from_today(date):
     convert_date = datetime.strptime(date, "%Y-%m-%d")
     today = datetime.today()
-    different = convert_date.date() - today.date()
-    return different
+    difference = convert_date.date() - today.date()
+    days_difference = difference.days
+    return days_difference
 
 
 print(get_days_from_today("2024-09-09"))
