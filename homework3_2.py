c57154446b5d3a2599900c3773c83636c6439ad0
Sv-Dan1k <164805682+Sv-Dan1k@users.noreply.github.com>
2024-04-28 00:01:48 +0300

    Update homework3_2.py

--- homework3_2.py
+++ homework3_2.py
@@ -1,9 +1,9 @@
 import random
 
-def get_numbers_ticket(ticket_input, min, max, quantity):
+def get_numbers_ticket(min, max, quantity):
     if len(ticket_input) != quantity:
         print("Invalid number of inputs")
-        return    
+        return [] 
     
     ticket_lottery = random.sample(range(min, max), quantity)
     ticket_lottery.sort()
@@ -16,11 +16,13 @@ def get_numbers_ticket(ticket_input, min, max, quantity):
         if lottery == user_choice:
             points += 1
     print('\nTotal points:', points)
+    return ticket_lottery
     
     
 print("Provide 5 numbers from 1 to 36 (one by one - separated by spaces):")
 ticket_input = list(map(int, input().split()))
 
 
-get_numbers_ticket(ticket_input, min=1, max=36, quantity=5)
+result = get_numbers_ticket(min=1, max=36, quantity=5)   # Не знаю чи потрібна цей рядок, який повертаємо пустий список, якщо кількість вхідних даних некоректна
+print(result)
 
