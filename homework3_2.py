6d4dfa08af2094122f2f77ef26d1e846b979454f
Sv-Dan1k <164805682+Sv-Dan1k@users.noreply.github.com>
2024-04-28 10:21:01 +0300

    Update homework3_2.py

--- homework3_2.py
+++ homework3_2.py
@@ -1,28 +1,13 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    if len(ticket_input) != quantity:
-        print("Invalid number of inputs")
+     if min < 1 or max > 1000 or min >= max or quantity < 1 or quantity > (max - min + 1):
+        print("Передані некоректні параметри.")
         return [] 
-    
-    ticket_lottery = random.sample(range(min, max), quantity)
-    ticket_lottery.sort()
-    result = list(zip(ticket_lottery, ticket_input))
-    print(f'Lottery Ticket: {" ".join([str(i) for i in ticket_lottery])}')
-    print(f'User\'s Choice: {" ".join([str(i) for i in ticket_input])}')    
-    
-    points = 0
-    for lottery, user_choice in result:
-        if lottery == user_choice:
-            points += 1
-    print('\nTotal points:', points)
-    return ticket_lottery
-    
-    
-print("Provide 5 numbers from 1 to 36 (one by one - separated by spaces):")
-ticket_input = list(map(int, input().split()))
+     
+     numbers = random.sample(range(min, max + 1), quantity)
+     return sorted(numbers)  
 
 
-result = get_numbers_ticket(min=1, max=36, quantity=5)   # Не знаю чи потрібна цей рядок, який повертаємо пустий список, якщо кількість вхідних даних некоректна
-print(result)
-
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
